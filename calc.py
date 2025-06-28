a855faf850a5f94c0bab98a7ba60d12b8e81ac12
abhishek johns <abhishekjohns418@gmail.com>
2025-06-28 12:52:27 +0530

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,29 @@
+import math
+def add():
+    x=int(input("Enter the 1st number: "))
+    y=int(input("Enter the 2nd number: "))
+    s=x+y
+    print(f"The sum is {s}")
+    
+def mul():
+    x=int(input("Enter the 1st number: "))
+    y=int(input("Enter the 2nd number: "))
+    m=x*y
+    print(f"The multiplication is {m}")
+
+def sub():
+    x=int(input("Enter the 1st number: "))
+    y=int(input("Enter the 2nd number: "))
+    diff=x-y
+    print(f"The difference is {diff}")
+
+def sqrt():
+    y=int(input("Enter the 2nd number: "))
+    sqr=math.sqrt(y)
+    print(f"The sum is {sqr}")
+
+def division():
+    x=int(input("Enter the 1st number: "))
+    y=int(input("Enter the 2nd number: "))
+    d=x/y
+    print(f"The division is {d}")
